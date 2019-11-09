2c7c2690625649444ddafcb4d05a6559907257d8
Wenyuan Wu <wenyuan.wu@uzh.ch>
2019-11-10 00:32:11 +0100

    plot results

--- perceptron.py
+++ perceptron.py
@@ -1,14 +1,16 @@
 import numpy as np
 
+
 class Perceptron(object):
-    """
-    Perceptron classifier.
+    """Perceptron classifier.
+
     Parameters
     ------------
     eta : float
     Learning rate (between 0.0 and 1.0)
     n_iter : int
     Passes over the training dataset.
+
     Attributes
     -----------
     w_ : 1d-array
@@ -21,8 +23,8 @@ class Perceptron(object):
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        """
-        Fit training data.
+        """Fit training data.
+
         Parameters
         ----------
         X : {array-like}, shape = [n_samples, n_features]
@@ -31,6 +33,7 @@ class Perceptron(object):
         n_features is the number of features.
         y : array-like, shape = [n_samples]
         Target values.
+
         Returns
         -------
         self : object
@@ -40,22 +43,18 @@ class Perceptron(object):
 
         for _ in range(self.n_iter):
             errors = 0
-        for xi, target in zip(X, y):
-            update = self.eta * (target - self.predict(xi))
-            self.w_[1:] += update * xi
-            self.w_[0] += update
-            errors += int(update != 0.0)
-        self.errors_.append(errors)
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
         return self
 
     def net_input(self, X):
-        """
-        Calculate net input
-        """
+        """Calculate net input"""
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def predict(self, X):
-        """
-        Return class label after unit step
-        """
+        """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
